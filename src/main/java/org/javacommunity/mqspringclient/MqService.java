99fc24b0738faf3008b3b1d81259da87daea88e1
Navneet Rai <navneet.rai@smartcomputerlabs.com>
2023-11-13 23:26:49 +0530

    initial commit

--- src/main/java/org/javacommunity/mqspringclient/MqService.java
+++ src/main/java/org/javacommunity/mqspringclient/MqService.java
@@ -1,28 +1,30 @@
 package org.javacommunity.mqspringclient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.jms.JmsException;
-import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableJms
 public class MqService {
-    @Autowired
-    private JmsTemplate jmsTemplate;
+       @Autowired
+       private JmsTemplate jmsTemplate;
 
-    @RequestMapping("send")
-    private String sendMessage(){
-        try{
-            jmsTemplate.convertAndSend("","hello ms");
-            return "OK";
-        }catch(JmsException e){
-            e.printStackTrace();
-            return "fail";
-        }
-    }
-
+       @RequestMapping("send")
+       private String sendMessage(){
+           try{
+               jmsTemplate.convertAndSend("","hello ms");
+               return "OK";
+           }catch(JmsException e){
+               e.printStackTrace();
+               return "fail";
+           }
+       }
+   @RequestMapping(path="test", produces= MediaType.TEXT_PLAIN_VALUE)
+   public String test(){
+       System.out.println("Restful");
+       return "My First Restful Service Is Working";
+   }
 }
